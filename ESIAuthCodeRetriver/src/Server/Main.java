659834975a7d56b39ed1080e0e0cef2016fca70f
William Rantis <william.rantis.20@cnu.edu>
2022-03-23 14:07:30 -0400

    basic working

--- ESIAuthCodeRetriver/src/Server/Main.java
+++ ESIAuthCodeRetriver/src/Server/Main.java
@@ -1,20 +1,48 @@
 package Server;
+import java.util.HashMap;
 import java.net.InetAddress;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.UnknownHostException;
+import java.util.Map;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 public class Main {
 
 	public Main() throws UnknownHostException, IOException{
 		InetSocketAddress isa = new InetSocketAddress(InetAddress.getLocalHost(), 8865);
 		HttpServer http = HttpServer.create(isa, 2);
+		
+		HttpHandler stopServer = (HttpExchange exchange) -> {
+			Map<String, String> map = getQueryString(exchange.getRequestURI().getQuery());
+			String codeStr = map.get("code");
+			String html = String.format("<html><body><p>Hello! Your authorization code is: %s</p></body></html>", codeStr);
+			byte[] byteHtml = html.getBytes();
+			exchange.sendResponseHeaders(200, byteHtml.length);
+			OutputStream os = exchange.getResponseBody();
+			os.write(byteHtml, 0, byteHtml.length);
+			os.close();
+			http.stop(0);
+		};
+		http.createContext("/callback", stopServer);
 		http.start();
-		http.stop(0);
+		
+		
 		
 	}
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		new Main();
+	}
+	public static Map<String, String> getQueryString(String s) {
+		HashMap<String, String> hashMap = new HashMap<String, String>(1);
+		String[] qs = s.split("&");
+		for (String individual: qs) {
+			String[] firstKeySecondValue = individual.split("=");
+			hashMap.put(firstKeySecondValue[0], firstKeySecondValue[1]);
+		}
+		return hashMap;
 	}
 
 }
